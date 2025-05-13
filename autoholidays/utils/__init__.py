eeea0d9741a19e33512fe6362e792d8813999eac
ZenithClown <debmalyapramanik.005@gmail.com>
2025-05-13 21:24:45 +0530

    🩹 patch add init time option registrations for utils.leaves

---
+++ autoholidays/utils/__init__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+
+"""
+Utility Functions for the Auto Holidays Package
+
+The utility functions are intended to provide common tools and helpers
+while may/may not be directly related to the main functionality of the
+package but is related to planning of holidays.
+"""
+
+from autoholidays.utils.leaves import *
