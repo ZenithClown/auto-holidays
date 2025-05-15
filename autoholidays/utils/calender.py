959c7d17a5ce44b3f4248383d57c8d4e07e85cd6
E33605 <debmalyapramanik.005@gmail.com>
2025-05-15 14:21:58 +0530

    💣🎉 create utils.calender, move ENUMDays, MonthDayConstruct

---
+++ autoholidays/utils/calender.py
@@ -0,0 +1,76 @@
+# -*- encoding: utf-8 -*-
+
+"""
+Utility Calender Function for Auto-Holidays Module
+
+The calender is a special utility function and not to be confused with
+the :mod:`calender` which have a different functionality. The defined
+objects may/maynot be directly related to the python in-built
+module which deals with calender days and calculations around it.
+"""
+
+from enum import Enum
+from pydantic import Field, BaseModel
+
+from autoholidays.errors import InvalidDayNum
+
+class ENUMDays(Enum):
+    """
+    ENUM for the Days of the Week
+
+    The days of the week is zero-indexed and starts from Monday. The
+    values are in-sync with the :mod:`datetime` module in Python, thus
+    providing backward compatibility.
+
+    .. code-block:: python
+
+        ENUMDays.SUNDAY # get the object for Sunday
+
+        # to get/search for a particular day, use:
+        print(ENUMDays(2))
+        >> ENUMDays.WEDNESDAY
+
+    The enum provides to distinct useful methods - :attr:`.name` to
+    get the name defined in the enum and :attr:`.value` to get the
+    value associated with the enum.
+
+    Once the enum is extended to other pydantic base models, we can
+    directly use the value without field validation as the value is
+    already validated by the enum.
+    """
+
+    MONDAY    = 0
+    TUESDAY   = 1
+    WEDNESDAY = 2
+    THURSDAY  = 3
+    FRIDAY    = 4
+    SATURDAY  = 5
+    SUNDAY    = 6
+
+
+    @classmethod
+    def __override_error__(cls, value) -> None:
+        """
+        Raise an Custom Error if the Value is not in Range
+        """
+
+        raise InvalidDayNum(
+            "`%r` is not a Valid `%s`. Valid Values: `%s`" % (
+                value,
+                cls.__name__,
+                ", ".join([str(day.value) for day in cls])
+            )
+        )
+
+
+class MonthDayConstruct(BaseModel):
+    """
+    Create a Month-Day Constructor for Various Usages
+
+    The construct allows to define the day and month without the year
+    value. Thus, the class can be dynamically unpacked to the module
+    :mod:`datetime` to iterate over a range of dynamic year value.
+    """
+
+    day : int = Field(ge = 1, le = 31)
+    month : int = Field(ge = 1, le = 12)
--- autoholidays/utils/leaves.py
+++ autoholidays/utils/leaves.py
@@ -15,8 +15,6 @@ The module uses the :mod:`pydantic` and :mod:`enum` for a structured
 defination of leaves which can be extended to the API module.
 """
 
-from enum import Enum
-
 from pydantic import (
     Field,
     BaseModel,
@@ -24,69 +22,7 @@ from pydantic import (
     field_validator
 )
 
-from autoholidays.errors import InvalidDayNum
-
-
-class ENUMDays(Enum):
-    """
-    ENUM for the Days of the Week
-
-    The days of the week is zero-indexed and starts from Monday. The
-    values are in-sync with the :mod:`datetime` module in Python, thus
-    providing backward compatibility.
-
-    .. code-block:: python
-
-        ENUMDays.SUNDAY # get the object for Sunday
-
-        # to get/search for a particular day, use:
-        print(ENUMDays(2))
-        >> ENUMDays.WEDNESDAY
-
-    The enum provides to distinct useful methods - :attr:`.name` to
-    get the name defined in the enum and :attr:`.value` to get the
-    value associated with the enum.
-
-    Once the enum is extended to other pydantic base models, we can
-    directly use the value without field validation as the value is
-    already validated by the enum.
-    """
-
-    MONDAY    = 0
-    TUESDAY   = 1
-    WEDNESDAY = 2
-    THURSDAY  = 3
-    FRIDAY    = 4
-    SATURDAY  = 5
-    SUNDAY    = 6
-
-
-    @classmethod
-    def __override_error__(cls, value) -> None:
-        """
-        Raise an Custom Error if the Value is not in Range
-        """
-
-        raise InvalidDayNum(
-            "`%r` is not a Valid `%s`. Valid Values: `%s`" % (
-                value,
-                cls.__name__,
-                ", ".join([str(day.value) for day in cls])
-            )
-        )
-
-
-class MonthDayConstruct(BaseModel):
-    """
-    Create a Month-Day Constructor for Various Usages
-
-    The construct allows to define the day and month without the year
-    value. Thus, the class can be dynamically unpacked to the module
-    :mod:`datetime` to iterate over a range of dynamic year value.
-    """
-
-    day : int = Field(ge = 1, le = 31)
-    month : int = Field(ge = 1, le = 12)
+from autoholidays.utils.calender import ENUMDays, MonthDayConstruct
 
 
 class CompWeeklyLeave(BaseModel):
@@ -383,4 +319,4 @@ class CustomLeaves(BaseModel):
     @field_validator("n_credit_balance")
     @classmethod
     def __validate_credit_balance__(cls, values : list[int]) -> list[int]:
-        assert min(values) >= 0, "`%s` must be non-negative" % values
+        assert min(values) >= 0, f"All of {values} must be non-negative"
